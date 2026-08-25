7e53c617f6ea9c3d94bfa6331702bea2383c1e25
Sean Cotter <Seancotter04@gmail.com>
2026-08-24 22:00:35 -0400

    Removed unused commented code

--- main.cpp
+++ main.cpp
@@ -54,10 +54,7 @@ void reverseAll(Vectors&... vecs) {
 int main()
 {
 	SMA sma(0.01);
-
 	BackTester_SMA backTester;
-
-
 	csvData data;
 
 	std::println("Reading CSV file: {}", DATA_DIRECTORY);
@@ -144,49 +141,8 @@ int main()
 		}
 
 		std::println("Final Capital: {}", capital);
-		
-		//simpleMovingAverage(1.0, 2.0);
-		// Callback function logic here
+
 	});
 
-	//cpr::Response r = cpr::Get(cpr::Url{ "https://epic-clinking-curliness.ngrok-free.dev/users" });
-
-	//if (r.status_code == 200)
-	//{
-	//	std::println("Successfully sent request to the server. Status code: {}", r.status_code);
-	//}
-	//else
-	//{
-	//	std::println("Failed to send request to the server. Status code: {}", r.status_code);
-	//}
-
 	return 0;
-	//int clientId = 0;
-	//unsigned attempt = 0;
-	//std::println("Starting IBKR C++ Mean Reversion Application {}...", attempt);
-	//for (;;)
-	//{
-	//	++attempt;
-
-	//	CWMR_Client client;
-
-	//	client.connect("127.0.0.1", 7497, clientId);
-
-	//	while (client.isConnected())
-	//	{
-	//		client.processMessages();
-	//	}
-
-	//	if (attempt >= MAX_ATTEMPTS)
-	//	{
-	//		break;
-	//	}
-
-	//	std::println("Sleeping {} seconds before next attempt", SLEEP_TIME);
-	//	std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
-	//}
-
-	//std::println("Exiting IBKR C++ Mean Reversion Application after {} attempts", attempt);
-	//
-	//return 0;
 }
