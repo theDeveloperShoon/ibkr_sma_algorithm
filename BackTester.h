d09f86ef49be6ef242aa0f2b3dfeabf9ac0e6063
Sean Cotter <Seancotter04@gmail.com>
2026-08-27 19:28:27 -0400

    Added a readCSV overload.

--- BackTester.h
+++ BackTester.h
@@ -18,6 +18,10 @@ public:
 
 	template <typename T>
 	void readCSV(T& returnObject, const std::string& filename);
+
+	template <typename T>
+	void readCSV(T& returnObject, const std::string& filename, bool reverse);
+
 	void calculateMovingAverages(const std::vector<double>& data, std::size_t window, std::vector<double>& returnVector);
 	[[nodiscard]] std::vector<double> calculateMovingAverages(const std::vector<double>& data, std::size_t window);
 };
@@ -26,21 +30,36 @@ public:
 
 template <typename T>
 void BackTester_SMA::readCSV(T& returnObject, const std::string& filename) {
+	readCSV<T>(returnObject, filename, false);
+
+	// Read data from a CSV file for backtesting
+	//glz::read_csv<glz::colwise>(/*filename, columnCount*/);
+} 
+
+template <typename T>
+void BackTester_SMA::readCSV(T& returnObject, const std::string& filename, bool reverse) {
 	std::string buffer{};
 	auto ec = glz::read_file_csv<glz::colwise>(returnObject, filename, buffer);
-
 	if (ec) {
 		std::println(
-			std::cerr, 
-			"Error reading CSV file: {}. Error code: {}", 
-			filename, 
-			glz::format_error(ec,buffer)
+			std::cerr,
+			"Error reading CSV file: {}. Error code: {}",
+			filename,
+			glz::format_error(ec, buffer)
 		);
 	}
 	else {
 		std::println("CSV file read successfully: {}", filename);
 	}
 
-	// Read data from a CSV file for backtesting
-	//glz::read_csv<glz::colwise>(/*filename, columnCount*/);
-} 
+	if (reverse) {
+		// Reverse the data in the returnObject
+		glz::for_each_field(returnObject, [](auto& field) {
+			using FIELD_TYPE = std::remove_cvref_t<decltype(field)>;
+
+			if constexpr (std::ranges::bidirectional_range<FIELD_TYPE>) {
+				std::ranges::reverse(field);
+			}
+		});
+	}
+}
